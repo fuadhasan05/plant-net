274a886da8b6e8a3215e2aa5d4e249041d70d94c
Fuad Hasan <fuadhasan3503@gmail.com>
2025-07-25 05:32:30 +0600

    handle useRole useing tanstack query

--- client/src/hooks/useRole.jsx
+++ client/src/hooks/useRole.jsx
@@ -1,24 +1,40 @@
-import { useState } from "react";
+// import { useState } from "react";
 import useAuth from "./useAuth";
 import useAxiosSecure from "./useAxiosSecure";
-import { useEffect } from "react";
+// import { useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 const useRole = () => {
-  const { user } = useAuth();
-  const [ role, setRole ] = useState(null);
-  const [isRoleLoading, setIsRoleLoading] = useState(true);
+  const { user, loading } = useAuth();
+  // const [ role, setRole ] = useState(null);
+  // const [isRoleLoading, setIsRoleLoading] = useState(true);
   const axiosSecure = useAxiosSecure();
-  useEffect(() => {
-    const fetchUserRole = async () => {
+
+  // tanstack query
+  const { data: role, isLoading: isRoleLoading } = useQuery({
+    queryKey: ["userRole", user?.email],
+    enabled: !loading && !!user?.email,
+    queryFn: async () => {
       const { data } = await axiosSecure(
         `${import.meta.env.VITE_API_URL}/user/role/${user?.email}`
       );
-      setRole(data?.role);
-      setIsRoleLoading(false);
-    };
-    fetchUserRole();
-  }, [user, axiosSecure]);
-  return [role, isRoleLoading];
+      return data;
+    },
+  });
+  console.log(role, isRoleLoading);
+
+  // useEffect(() => {
+  //   const fetchUserRole = async () => {
+  //     const { data } = await axiosSecure(
+  //       `${import.meta.env.VITE_API_URL}/user/role/${user?.email}`
+  //     );
+  //     setRole(data?.role);
+  //     setIsRoleLoading(false);
+  //   };
+  //   fetchUserRole();
+  // }, [user, axiosSecure]);
+
+  return [role?.role, isRoleLoading];
 };
 
 export default useRole;
